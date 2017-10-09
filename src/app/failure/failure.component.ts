b7a64e13674986df3719947e34c07402abe782da
John Goff <gygn.johngoff@gmail.com>
2017-10-09 17:05:52 -0400

    Add back button to success and failure

--- src/app/failure/failure.component.ts
+++ src/app/failure/failure.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-failure',
   templateUrl: './failure.component.html',
   styleUrls: ['./failure.component.css']
 })
-export class FailureComponent implements OnInit {
+export class FailureComponent {
+  constructor(private router: Router) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  goBack(): void {
+    this.router.navigateByUrl('');
   }
-
 }
--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-success',
   templateUrl: './success.component.html',
   styleUrls: ['./success.component.css']
 })
-export class SuccessComponent implements OnInit {
+export class SuccessComponent {
+  constructor(private router: Router) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  goBack(): void {
+    this.router.navigateByUrl('');
   }
-
 }
